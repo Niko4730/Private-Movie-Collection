7f4c524c9d57e676e20343d60f391beb6cbd261a
Onii-chan <failer007@live.dk>
2021-01-04 11:23:32 +0100

    Added mysql sql

---
+++ Data/sql/privatemoviecollection_mysql.sql
@@ -0,0 +1,68 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Local MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 100417
+ Source Host           : localhost:3306
+ Source Schema         : privatemoviecollection
+
+ Target Server Type    : MySQL
+ Target Server Version : 100417
+ File Encoding         : 65001
+
+ Date: 04/01/2021 11:21:43
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`category_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for movie
+-- ----------------------------
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE `movie`  (
+  `movie_id` int(11) NOT NULL AUTO_INCREMENT,
+  `movie_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `movie_file` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `movie_lastview` int(11) NULL DEFAULT NULL,
+  `rating_id` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`movie_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for movie_collection
+-- ----------------------------
+DROP TABLE IF EXISTS `movie_collection`;
+CREATE TABLE `movie_collection`  (
+  `category_id` int(11) NOT NULL,
+  `movie_id` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`category_id`) USING BTREE,
+  INDEX `movie_id`(`movie_id`) USING BTREE,
+  CONSTRAINT `movie_collection_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for rating
+-- ----------------------------
+DROP TABLE IF EXISTS `rating`;
+CREATE TABLE `rating`  (
+  `rating_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rating_amount` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `movie_id` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`rating_id`) USING BTREE,
+  INDEX `movie_id`(`movie_id`) USING BTREE,
+  CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
